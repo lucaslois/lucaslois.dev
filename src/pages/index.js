8aed4065739cdf76007166d21ead57a0991dcac4
Lucky <lucas@braintly.com>
2023-04-25 20:11:48 -0300

    Updated Gatsby and added my resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,30 @@
 import * as React from "react"
 import '../app.scss';
 
+import MyResumePDF from '../Lucas_Lois_Technical_Leader_CV.pdf'
+import LuckyImage from '../images/lucky.jpeg'
+
 // markup
 const IndexPage = () => {
     return (
         <main className='main div100vh'>
             <div className='personal-info'>
-                <img className='photo' src="https://avatars.githubusercontent.com/u/14177309?v=4" alt=""/>
+                <img className='photo' src={LuckyImage} alt="Lucky Avatar"/>
                 <title>Lucas Lois</title>
-                <h1 className='title'>Lucas <span className='highlight'>"Lucky"</span> Lois</h1>
+                <h1 className='title'>Lucas Lois</h1>
                 <h2 className='subtitle'>Software Engineer & Architect</h2>
                 <h3 className='job'>Technical Leader at <a target='_blank' href="https://braintly.com">Braintly</a></h3>
+                <h4 className='resume'><a href={MyResumePDF} target="_blank">My Resume</a></h4>
             </div>
             <div className="buttons">
+                <a target='_blank' href='https://www.linkedin.com/in/lucaslois/' className='button linkedin'>Linked
+                    In</a>
                 <a target='_blank' href='https://github.com/lucaslois' className='button github'>GitHub</a>
                 <a target='_blank' href='https://instagram.com/lucaslois95' className='button instagram'>Instagram</a>
                 <a target='_blank' href='https://twitter.com/lucaslois95' className='button twitter'>Twitter</a>
-                <a target='_blank' href='https://www.linkedin.com/in/lucaslois/' className='button linkedin'>Linked
-                    In</a>
-                <a target='_blank' href='/books' className='button books'>Recommended Books</a>
-                <a target='_blank' className='button disabled'>Recommended Tools (Coming soon)</a>
-                <a target='_blank' className='button disabled'>My Setup (Coming soon)</a>
+                {/* <a target='_blank' href='/books' className='button books'>Recommended Books</a> */}
+                {/* <a target='_blank' className='button disabled'>Recommended Tools (Coming soon)</a> */}
+                {/* <a target='_blank' className='button disabled'>My Setup (Coming soon)</a> */}
             </div>
         </main>
     )
